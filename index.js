503ac74c05b39deb2cd46b5e1ace9d86ec8db7bf
Priyanka-Agrawal2022 <prnkgrwl286@gmail.com>
2023-06-05 04:07:44 +0530

    set up static files and create a view for home

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use('/', require('./routes'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// set up static files
+app.use(express.static('./assets'));
+
 app.listen(port, function(err) {
     if(err) {
         console.log(`Error in running the server: ${err}`);
